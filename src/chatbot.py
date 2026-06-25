9bd673e0b190fc62d26a72e1dd03926394997bbf
imen_hammami <imen.hammami@esprit.tn>
2026-06-25 12:17:10 +0100

    le cœur du chatbot

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,1 +1,10 @@
 print("🤖 Chatbot démarré ! Tape 'exit' pour quitter.")
+
+while True:
+    user_input = input("Toi: ")
+
+    if user_input == "exit":
+        print("Bot: Au revoir !")
+        break
+
+    print("Bot: Je t'ai entendu !")
